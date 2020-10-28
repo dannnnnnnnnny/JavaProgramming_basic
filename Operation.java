a5497cf5df1916eadc02082d9051146457104fcb
dh_kim <ehdgnl5249@naver.com>
2020-10-28 16:56:25 +0900

    Number Operation

--- Operation.java
+++ Operation.java
@@ -5,7 +5,14 @@ public class Operation {
         System.out.println("6"); // String 6
         System.out.println(6 + 6); // Number 12
         System.out.println("6" + "6"); // String 66
-
         System.out.println("1111".length()); // 4
+
+        System.out.println("--------------------------");
+        System.out.println(6 + 2); // 8
+        System.out.println(6 - 2); // 4
+        
+        System.out.println(Math.PI); // 3.1415926535
+        System.out.println(Math.floor(Math.PI)); //
+        System.out.println(Math.ceil(Math.PI));
     }
 }
