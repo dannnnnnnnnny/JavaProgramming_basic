ef70428bec632f3600faf2e3ab5f44c4327b209a
dh_kim <ehdgnl5249@naver.com>
2020-10-28 16:51:35 +0900

    Number & String

---
+++ Operation.java
@@ -0,0 +1,11 @@
+public class Operation {
+    public static void main(String[] args) {
+        System.out.println(6); // Number
+        System.out.println("six"); // String
+        System.out.println("6"); // String 6
+        System.out.println(6 + 6); // Number 12
+        System.out.println("6" + "6"); // String 66
+
+        System.out.println("1111".length()); // 4
+    }
+}
